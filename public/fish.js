d68a6b92579b0659965355b159d774d54f6f8c63
weshays <weshays@gbdev.com>
2016-11-04 22:24:50 -0700

    Added some updates, and did some refactoring.

--- public/fish.js
+++ public/fish.js
@@ -1,7 +1,7 @@
 BetaFish = {
   defaults : { 'access_key' : null },
-  class_vars : { 'url' : 'https://betafish.herokuapp.com/api/feedback_entries' },
-  //class_vars : { 'url' : 'http://localhost:3000/api/feedback_entries' },
+  //class_vars : { 'url' : 'https://betafish.herokuapp.com/api/feedback_entries' },
+  class_vars : { 'url' : 'http://localhost:3000/api/feedback_entries' },
 
   show_form : function() {
 
@@ -13,20 +13,22 @@ BetaFish = {
 
   take_snapshot : function() {
     var self = this;
+
     html2canvas(document.body, {
-      onrendered: function(canvas) {
-        var data = { 'feedback_entry' : {
-          'comprehension' : 'happy',
-          'path' : window.location.href,
-          'comment' : 'This is really awesome',
-          'browser' : navigator.userAgent,
-          'operating_system' : navigator.platform,
-          'screen_resolution' : window.screen.width + ' x ' + window.screen.height,
-          'browser_resolution' : document.body.clientWidth + ' x ' + document.body.clientHeight,
-          'screenshot' : canvas.toDataURL('image/png') //.split(',')[1]
-        }};
-        self.post_feedback(data);
-      }
+      logging: true
+    }).then(function(canvas) {
+      // document.body.appendChild(canvas);
+      var data = { 'feedback_entry' : {
+        'comprehension' : 'happy',
+        'path' : window.location.href,
+        'comment' : 'This is really awesome',
+        'browser' : navigator.userAgent,
+        'operating_system' : navigator.platform,
+        'screen_resolution' : window.screen.width + ' x ' + window.screen.height,
+        'browser_resolution' : document.body.clientWidth + ' x ' + document.body.clientHeight,
+        'screenshot' : canvas.toDataURL('image/png') //.split(',')[1]
+      }};
+      self.post_feedback(data);
     });
   },
 
@@ -37,8 +39,7 @@ BetaFish = {
     xmlDoc.setRequestHeader('Content-Type', 'application/json');
     xmlDoc.onreadystatechange = function() {
       if (xmlDoc.readyState === 4 && xmlDoc.status === 200) {
-        // callback(xmlDoc);
-        console.log('ready....');
+        // console.log('ready....');
       }
     }
     xmlDoc.send(JSON.stringify(data));
